adaaea26b51f992c49af8e7ec2410f9cda7e758e
Tobias Pickel <tobias.pickel@xing.com>
2020-04-21 16:44:44 +0200

    Updates

---
+++ precache-manifest.67e87f9a9e4ccbf64b127e1dd4607b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5db6b01f6b50881d6b8eb3da3903574",
+    "url": "/aws-stift-und-block/index.html"
+  },
+  {
+    "revision": "2a70cea0f4908d3e310c",
+    "url": "/aws-stift-und-block/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2cc5d4a8900e46c57491",
+    "url": "/aws-stift-und-block/static/js/2.db675918.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/aws-stift-und-block/static/js/2.db675918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a70cea0f4908d3e310c",
+    "url": "/aws-stift-und-block/static/js/main.ea3f3e00.chunk.js"
+  },
+  {
+    "revision": "0d331f83257944a3f035",
+    "url": "/aws-stift-und-block/static/js/runtime-main.d2e4e5cc.js"
+  }
+]);
